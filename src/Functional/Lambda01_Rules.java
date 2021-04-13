cadfba89dbcbb0d424a2727fe517f3556957ec1d
SerhatDuzen <serhatduzen3gmail.com>
2021-04-13 13:49:59 +0300

    13.04.2020

---
+++ src/Functional/Lambda01_Rules.java
@@ -0,0 +1,14 @@
+package Functional;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda01_Rules {
+    public static void main(String[] args) {
+        List<Integer> liste = Arrays.asList(1,2,3,4,5);
+        liste.forEach(i-> System.out.println(i));
+    }
+
+
+
+}
